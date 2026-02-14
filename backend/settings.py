a9b90e1e1551031b6cf9aef680ee6eee71252c58
jimcarey1 <89066752+jimcarey1@users.noreply.github.com>
2026-02-14 12:07:28 +0530

    Converting ACCESS_TOKEN_TTL and REFRESH_TOKEN_TTL to integers after loading from .env.

--- backend/settings.py
+++ backend/settings.py
@@ -4,5 +4,5 @@ import os
 load_dotenv()
 
 TOKEN_SECRET = os.getenv('TOKEN_SECRET')
-REFRESH_TOKEN_TTL = os.getenv('REFRESH_TOKEN_TTL')
-ACCESS_TOKEN_TTL = os.getenv('ACCESS_TOKEN_TTL')
+REFRESH_TOKEN_TTL = int(os.getenv('REFRESH_TOKEN_TTL'))
+ACCESS_TOKEN_TTL = int(os.getenv('ACCESS_TOKEN_TTL'))
