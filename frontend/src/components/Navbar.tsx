171f8a806db95e15a2ad4c0c07c3b6b5c244cd7b
jimcarey1 <89066752+jimcarey1@users.noreply.github.com>
2026-02-19 09:29:53 +0530

    Worked on navbar component and included it in the homepage

---
+++ frontend/src/components/Navbar.tsx
@@ -0,0 +1,35 @@
+import '../styles/navbar.css'
+
+const Navbar = ()=>{
+    return (
+        <nav className="navbar">
+            <div className="project-name">
+                <p>reddit</p>
+            </div>
+            <div className="search-bar">
+                <i className="bi bi-reddit"></i>
+                <input type="text" placeholder="Find Anything" />
+            </div>
+            <div className="action-buttons">
+                <div className="advertisment-icon">
+                    <i className="bi bi-badge-ad"></i>
+                </div>
+                <div className="chat-icon">
+                    <i className="bi bi-chat-dots"></i>
+                </div>
+                <div className="create-post-icon">
+                    <i className="bi bi-plus-square"></i>
+                    <p>create</p>
+                </div>
+                <div className="notification-icon">
+                    <i className="bi bi-bell"></i>
+                </div>
+                <div className="profile-icon">
+                    <i className="bi bi-person"></i>
+                </div>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
---
+++ frontend/src/pages/HomePage.tsx
@@ -0,0 +1,11 @@
+import Navbar from "../components/Navbar"
+
+const HomePage = ()=>{
+    return (
+        <main className="home-container">
+            <Navbar />
+        </main>
+    )
+}
+
+export default HomePage
