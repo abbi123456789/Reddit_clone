8c2dbf8c7c82ceac03b8c8ee3918b572944d6645
jimcarey1 <89066752+jimcarey1@users.noreply.github.com>
2026-02-19 09:30:19 +0530

    Added a route for HomePage

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,13 @@
 import { Routes, Route } from "react-router-dom"
 import Registration from "./pages/RegistrationPage"
 import Login from "./pages/LoginPage"
+import HomePage from "./pages/HomePage"
 
 const App = ()=>{
   return (
     <main>
       <Routes>
+        <Route path="/" element={<HomePage />} />
         <Route path="/register" element={<Registration />} />
         <Route path="/login" element={<Login />} />
       </Routes>
