bbd3259268b3662f28aabd20d94c2fee92e5e320
Viswanadh <viswanadhlingareddy@gmail.com>
2026-02-23 16:37:23 +0530

    added community routes to main application

--- backend/main.py
+++ backend/main.py
@@ -3,6 +3,7 @@ from litestar.openapi.config import OpenAPIConfig
 from litestar.config.cors import CORSConfig
 
 from accounts.controllers import jwt_auth, UserController
+from community.controllers import CommunityController
 
 cors_config = CORSConfig(
     allow_origins=['http://localhost:5173'],
@@ -16,7 +17,7 @@ openapi_config = OpenAPIConfig(
 )
 
 app = Litestar(
-    route_handlers=[UserController],
+    route_handlers=[UserController, CommunityController],
     on_app_init=[jwt_auth.on_app_init],
     openapi_config=openapi_config,
     cors_config = cors_config,
