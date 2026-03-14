29705c3d116ddb2a2312504433d25bd6c49a37b6
Viswanadh <viswanadhlingareddy@gmail.com>
2026-03-14 19:54:34 +0530

    Implement community and flair selection in PostCreationForm component

--- frontend/src/components/post/PostCreationForm.tsx
+++ frontend/src/components/post/PostCreationForm.tsx
@@ -1,5 +1,52 @@
 import RichTextEditor from "../Editor/RichTextEditor"
+import { getMyCommunities } from "../../services/community"
+import { getFlairs } from "../../services/flairs"
+import { useEffect, useState } from "react"
+
+export type Community = {
+    id: number
+    name: string
+}
+
+export type Flair = {
+    id: number
+    title: string
+    background_color: string
+    text_color: string
+}
+
 const PostCreationForm = ()=>{
+    const [myCommunities, setMyCommunities] = useState<Community[]>([])
+    const [flairs, setFlairs] = useState<Flair[]>([])
+
+    const [selectedCommunity, setSelectedCommunity] = useState("")
+    const [title, setTitle] = useState("")
+    const [selectedFlair, setSelectedFlair] = useState("")
+
+    useEffect(()=>{
+        const fetchCommunities = async()=>{
+            try{
+                const communities = await getMyCommunities()
+                setMyCommunities(communities)
+            }catch(error){
+                console.error("Error fetching communities:", error)
+            }
+        }
+        fetchCommunities()
+    },[])
+
+    const handleCommunitySelection = async (event: React.ChangeEvent<HTMLSelectElement>)=>{
+        const communityName = event.target.value
+        setSelectedCommunity(communityName)
+        try{
+            const flairs = await getFlairs(communityName)
+            setFlairs(flairs)
+            console.log(flairs)
+        }catch(error){
+            console.error("Error fetching flairs:", error)
+        }
+    }
+
     return(
         <section className="create-post-section">
             <div className="create-post-desc">
@@ -8,25 +55,28 @@ const PostCreationForm = ()=>{
             </div>
 
             <div className="community-selection">
-                <select name="choose-community">
+                <select name="choose-community" onChange={handleCommunitySelection} value={selectedCommunity}>
                     <option value=''>Select a community</option>
-                    <option value='AskDocs'>r/AskDocs</option>
-                    <option value='aws'>r/aws</option>
-                    <option value='webdev'>r/webdev</option>
-                    <option value='devops'>r/devops</option>
+                    {myCommunities.map((community)=>(
+                        <option key={community.id} value={community.name}>
+                            {community.name}
+                        </option>
+                    ))}
                 </select>
             </div>
 
             <div className="post-title">
-                <input type="text" placeholder="Title" required />
+                <input type="text" placeholder="Title" value={title} onChange={(e)=>setTitle(e.target.value)} required />
             </div>
 
             <div className="add-flair-and-tag">
-                <select name="flair-tag">
+                <select name="flair-tag" value={selectedFlair} onChange={(e)=>setSelectedFlair(e.target.value)}>
                     <option value=''>Select a flair</option>
-                    <option value='flair1'>Flair1</option>
-                    <option value='flair2'>Flair2</option>
-                    <option value='flair3'>Flair3</option>
+                    {flairs.map((flair)=>(
+                        <option key={flair.id} value={flair.title}>
+                            {flair.title}
+                        </option>
+                    ))}
                 </select>
             </div>
 
