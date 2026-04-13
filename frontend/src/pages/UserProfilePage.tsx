c385b88ada86cda6ba5e5c09b86d294d66298c3b
Viswanadh <viswanadhlingareddy@gmail.com>
2026-04-13 20:17:49 +0530

    feat: create UserProfilePage component with Navbar, Sidebar, and UserActivity integration

---
+++ frontend/src/pages/UserProfilePage.tsx
@@ -0,0 +1,24 @@
+import { UserActivity } from '../components/profile/UserActivity';
+import Navbar from '../components/Navbar';
+import Sidebar from '../components/Sidebar';
+
+type UserProfilePageProps = {
+    showModal: boolean
+    handleModalToggle: () => void
+}
+
+const UserProfilePage = ({ showModal, handleModalToggle }: UserProfilePageProps) => {
+    return (
+        <main className="community-container">
+            <Navbar />
+            <div className="main-content">
+                <Sidebar showModal={showModal} handleModalToggle={handleModalToggle} />
+                <div className="sub-content">
+                    <UserActivity />
+                </div>
+            </div>
+        </main>
+    )
+}
+
+export default UserProfilePage;
