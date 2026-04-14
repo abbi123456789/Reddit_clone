5fe8d53590e39e279184115357a3bb6297aad535
jimcarey1 <89066752+jimcarey1@users.noreply.github.com>
2026-04-14 15:24:16 +0530

    refactor: added async rendering, old content will show up while loading new tab.

--- frontend/src/components/profile/UserActivity.tsx
+++ frontend/src/components/profile/UserActivity.tsx
@@ -1,8 +1,20 @@
 import {Tabs, TabList, Tab, TabPanels, TabPanel} from '../../components/ui/Tabs'
+import { Suspense, useState, useTransition } from 'react'
+import type { Key } from 'react-aria-components'
 
 export function UserActivity () {
+    const [activeTab, setActiveTab] = useState<Key>('posts')
+    const [isPending, startTransition] = useTransition()
+
+    const handleTabChange = (key: Key) => {
+        startTransition(async ()=>{
+            setActiveTab(key)
+            await new Promise((resolve)=>setTimeout(resolve, 1000))
+            console.log('Hello beautiful.')
+        })
+    }
     return (
-        <Tabs style={{width: '100%'}}>
+        <Tabs selectedKey={activeTab} onSelectionChange={handleTabChange} style={{width: '100%'}}>
             <div style={{display: 'flex' }}>
                 <TabList>
                     <Tab id='posts'>Posts</Tab>
@@ -11,18 +23,26 @@ export function UserActivity () {
                     <Tab id='downvoted'>Downvoted</Tab>
                 </TabList>
             </div>
-            <TabPanels>
+            <TabPanels style={{opacity: isPending ? 0.5 : 1, transition: 'opacity 0.4s linear'}}>
                 <TabPanel id='posts'>
-                    <p>Posts content goes here...</p>
+                    <Suspense fallback={null}>
+                        <p>Posts content goes here...</p>
+                    </Suspense>
                 </TabPanel>
                 <TabPanel id='comments'>
-                    <p>Comments content goes here...</p>
+                    <Suspense fallback={null}>
+                        <p>Comment content goes here...</p>
+                    </Suspense>
                 </TabPanel>
                 <TabPanel id='upvoted'>
-                    <p>Upvoted content goes here...</p>
+                    <Suspense fallback={null}>
+                        <p>Upvoted content goes here...</p>
+                    </Suspense>
                 </TabPanel>
                 <TabPanel id='downvoted'>
-                    <p>Downvoted content goes here...</p>
+                    <Suspense fallback={null}>
+                        <p>Downvoted content goes here...</p>
+                    </Suspense>
                 </TabPanel>
             </TabPanels>
         </Tabs>
