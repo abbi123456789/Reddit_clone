3f6eaf00326d71340ce53e5d2d44e5e859663675
jimcarey1 <89066752+jimcarey1@users.noreply.github.com>
2026-04-15 18:00:34 +0530

    refactor: fetching voted posts based on the value query parameter. Removed getUserDownvotedPosts function and renamed getUserUpvotedPosts to getUserVotedPosts.

--- frontend/src/services/profile.ts
+++ frontend/src/services/profile.ts
@@ -25,16 +25,12 @@ export async function getUserPosts() : Promise<UserPosts[]>{
 }
 
 export async function getUserComments() : Promise<UserComments[]>{
+    await new Promise((resolve)=>setTimeout(resolve, 500))
     const response = await api.get('/profile/comments')
     return response.data
 }
 
-export async function getUserUpvotedPosts() : Promise<UserPosts[]> {
-    const response = await api.get('/profile/upvoted-posts')
-    return response.data
-}
-
-export async function getUserDownvotedPosts() : Promise<UserPosts[]> {
-    const response = await api.get('/profile/downvoted-posts')
+export async function getUserVotedPosts(value: number) : Promise<UserPosts[]> {
+    const response = await api.get(`/profile/voted-posts/?value=${value}`)
     return response.data
 }
