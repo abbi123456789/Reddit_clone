3c310a7952ac18b7f2378854b5ffaeb0f9adf0ba
jimcarey1 <89066752+jimcarey1@users.noreply.github.com>
2026-04-21 16:43:54 +0530

    refactor: Lazy loading components using suspense and lazy imports.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,20 +1,20 @@
+import React from "react"
 import { Routes, Route } from "react-router-dom"
-import Registration from "./pages/RegistrationPage"
-import Login from "./pages/LoginPage"
-import HomePage from "./pages/HomePage"
-import CommunityPage from "./pages/CommunityPage"
-import CreatePost from "./pages/CreatePost"
-import LookAndFeelPage from "./pages/LookAndFeelPage"
-import PostFlairPage from "./pages/PostFlairPage"
-import PostDetail from "./pages/PostDetail"
-import ModeratorPostsAndComments from "./pages/ModeratorPostsAndComments"
-import ModeratorGuides from "./pages/ModeratorGuides"
-import ModeratorSettings from "./pages/ModeratorSettings"
-import UserProfilePage from "./pages/UserProfilePage"
-import { useState } from "react"
+const Registration = React.lazy(()=>import('./pages/RegistrationPage'))
+const Login = React.lazy(()=>import('./pages/LoginPage'))
+const HomePage = React.lazy(()=>import('./pages/HomePage'))
+const CommunityPage = React.lazy(()=>import('./pages/CommunityPage'))
+const CreatePost = React.lazy(()=>import('./pages/CreatePost'))
+const LookAndFeelPage = React.lazy(()=>import('./pages/LookAndFeelPage'))
+const PostFlairPage = React.lazy(()=>import('./pages/PostFlairPage'))
+const PostDetail = React.lazy(()=>import('./pages/PostDetail'))
+const ModeratorPostsAndComments = React.lazy(()=>import('./pages/ModeratorPostsAndComments'))
+const ModeratorGuides = React.lazy(()=>import('./pages/ModeratorGuides'))
+const ModeratorSettings = React.lazy(()=>import('./pages/ModeratorSettings'))
+const UserProfilePage = React.lazy(()=>import('./pages/UserProfilePage'))
 
 const App = ()=>{
-  const [showModal, setShowModal] = useState<boolean>(false)
+  const [showModal, setShowModal] = React.useState<boolean>(false)
 
   const handleModalToggle = () => {
     console.log(showModal)
@@ -23,21 +23,23 @@ const App = ()=>{
 
   return (
     <main>
-      <Routes>
-        <Route path="/" element={<HomePage handleModalToggle = {handleModalToggle} showModal={showModal}/>} />
-        <Route path="/r/:communityName" element={<CommunityPage handleModalToggle={handleModalToggle} showModal={showModal} />} />
-        <Route path="/submit" element={<CreatePost handleModalToggle={handleModalToggle} showModal={showModal} />} />
-        <Route path="/r/:communityName/submit" element={<CreatePost handleModalToggle={handleModalToggle} showModal={showModal} />} />
-        <Route path="/r/:communityName/comments/:postId/:postSlug" element={<PostDetail handleModalToggle={handleModalToggle} showModal={showModal} />} />
-        <Route path="/register" element={<Registration />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/r/:communityName/look-feel" element={<LookAndFeelPage />} />
-        <Route path="/r/:communityName/post-flair" element={<PostFlairPage />} />
-        <Route path="/r/:communityName/posts-and-comments" element={<ModeratorPostsAndComments />} />
-        <Route path='/r/:communityName/guides' element={<ModeratorGuides />} />
-        <Route path='/r/:communityName/general-settings' element={<ModeratorSettings />} />
-        <Route path='/u/:username' element={<UserProfilePage handleModalToggle={handleModalToggle} showModal={showModal}/>} />
-      </Routes>
+      <React.Suspense>
+        <Routes>
+          <Route path="/" element={<HomePage handleModalToggle = {handleModalToggle} showModal={showModal}/>} />
+          <Route path="/r/:communityName" element={<CommunityPage handleModalToggle={handleModalToggle} showModal={showModal} />} />
+          <Route path="/submit" element={<CreatePost handleModalToggle={handleModalToggle} showModal={showModal} />} />
+          <Route path="/r/:communityName/submit" element={<CreatePost handleModalToggle={handleModalToggle} showModal={showModal} />} />
+          <Route path="/r/:communityName/comments/:postId/:postSlug" element={<PostDetail handleModalToggle={handleModalToggle} showModal={showModal} />} />
+          <Route path="/register" element={<Registration />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/r/:communityName/look-feel" element={<LookAndFeelPage />} />
+          <Route path="/r/:communityName/post-flair" element={<PostFlairPage />} />
+          <Route path="/r/:communityName/posts-and-comments" element={<ModeratorPostsAndComments />} />
+          <Route path='/r/:communityName/guides' element={<ModeratorGuides />} />
+          <Route path='/r/:communityName/general-settings' element={<ModeratorSettings />} />
+          <Route path='/u/:username' element={<UserProfilePage handleModalToggle={handleModalToggle} showModal={showModal}/>} />
+        </Routes>
+      </React.Suspense>
     </main>
   )
 }
